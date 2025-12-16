766f206f63bd9dcb21d69f910f14b03a155816c6
Jamir Marzál Pérez <jamirmarzalperez@gmail.com>
2025-12-16 11:08:30 -0500

    chore: update the project enviroment variables

--- src/config.py
+++ src/config.py
@@ -6,12 +6,14 @@ class Config:
     
     YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
     
-    PROJECT_ID = os.getenv('PROJECT_ID')
-    DATASET_ID = os.getenv('DATASET_ID', 'youtube_data')
-    CREDENTIALS_PATH = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    DB_HOST = os.getenv("YT_DB_HOST", "youtube-postgres")
+    DB_PORT = int(os.getenv("YT_DB_PORT", 5432))
+    DB_NAME = os.getenv("YT_DB_NAME", "youtube_analytics")
+    DB_USER = os.getenv("YT_DB_USER", "yt_user")
+    DB_PASSWORD = os.getenv("YT_DB_PASSWORD", "yt_pass")    
     
     MAX_VIDEOS = int(os.getenv('MAX_VIDEOS', 50))
-    MAX_COMMENTARIOS = int(os.getenv('MAX_COMMENTS', 100))
+    MAX_COMENTARIOS = int(os.getenv('MAX_COMENTARIOS', 100))
     EXTRACT_COMMENTS = os.getenv('EXTRACT_COMMENTS', 'True').lower() == 'true'
     
     DATA_DIR = os.getenv('DATA_DIR', '/opt/airflow/data')
